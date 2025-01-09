070eb17ab24061fe6636a16ea9e1d595693d7726
Jhanvi Chaudhary <70269514+jhanvi2903@users.noreply.github.com>
2025-01-09 17:10:34 +0000

    Time: 0 ms (100%), Space: 42.2 MB (10.24%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.java
+++ 0101-symmetric-tree/0101-symmetric-tree.java
@@ -19,14 +19,21 @@ class Solution {
     }
 
     public boolean helper(TreeNode leftTree, TreeNode rightTree){
+        // If both the subtree are null, then symmetric 
         if(leftTree == null && rightTree == null){
             return true;
         }
 
+        // If any node among them is null, then not symmetric
         if(leftTree == null || rightTree == null){
             return false;
         }
 
+    // Recursive Case: Both nodes are not null:
+    // If both nodes are non-null, we compare their values (leftTree.val == rightTree.val).
+    // Then, we make two recursive calls to compare the following:
+    // The left child of the left subtree with the right child of the right subtree 
+    // The right child of the left subtree with the left child of the right subtree 
         return (leftTree.val == rightTree.val) && helper(leftTree.left, rightTree.right) && helper(leftTree.right, rightTree.left);
     }
 }
