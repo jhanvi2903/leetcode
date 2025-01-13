6030fb714a996b1c54ee4ce6b7f6a756018ea3b4
Jhanvi Chaudhary <70269514+jhanvi2903@users.noreply.github.com>
2025-01-13 20:09:48 +0000

    Time: 0 ms (100%), Space: 41.8 MB (39.4%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
@@ -22,14 +22,14 @@ class Solution {
         while(true){
             if(node != null){
                 st.push(node);
-                node = node.left;
+                node = node.left; // keep moving to the left until it points to the null and add the node to stack
             }else{
-                if(st.isEmpty()){
+                if(st.isEmpty()){ // If stack becomes empty, then the entire binary tree is traversed
                     break;
                 }
-                node = st.pop();
+                node = st.pop();  // When the node becomes null, pop the element from stack and store its value in list 
                 result.add(node.val);
-                node = node.right;
+                node = node.right; // Then move to right
             }
         }
        
