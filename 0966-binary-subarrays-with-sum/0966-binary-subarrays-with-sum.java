1c8d97ca6621d0694d01f9a6563318e53aa46635
Jhanvi Chaudhary <70269514+jhanvi2903@users.noreply.github.com>
2025-01-19 18:00:19 +0000

    Time: 1190 ms (11.35%), Space: 47.4 MB (82.31%) - LeetHub

--- 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.java
+++ 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.java
@@ -3,6 +3,7 @@ class Solution {
 
         int subarrayCount = 0;
 
+        // Brute force approach
         for(int i = 0; i < nums.length; i++){
             int sum = 0;
             for(int j = i; j < nums.length; j++){
