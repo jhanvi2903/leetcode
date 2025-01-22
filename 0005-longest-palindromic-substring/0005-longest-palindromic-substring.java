67ae204832e13ee75c51ba8f0b78e6a9184acf44
Jhanvi Chaudhary <70269514+jhanvi2903@users.noreply.github.com>
2025-01-22 13:41:30 +0000

    Time: 15 ms (86.18%), Space: 42.3 MB (75.19%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -0,0 +1,39 @@
+class Solution {
+    public String longestPalindrome(String s) {
+        // Types of Palindrome 
+        // 1. Odd length - aba
+        // 2. Even length - abba
+        // Considering i at b in both the ways
+        // In first case, it will compare with itself and move to the left & right
+        // In second case, it compares with the next b, as they are equal, left moves to left & right moves to right
+        if(s == null || s.length() == 1){
+            return s;
+        } 
+
+// To maintain the end of the longest substring palindrome
+        int start = 0;
+        int end = 0;
+
+        for(int i = 0; i < s.length(); i++){
+            int odd = expandAroundCenter(s, i, i);
+            int even = expandAroundCenter(s, i, i+1);
+            int maxLen = Math.max(odd, even);
+
+            if(maxLen > end - start){
+                start = i - (maxLen - 1)/2;
+                end = i + maxLen/2;
+            }
+        }
+        
+        return s.substring(start, end + 1);
+    }
+
+    private int expandAroundCenter(String s, int left, int right){
+        while(left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)){
+            left--;
+            right++;
+        }
+
+        return right - left - 1;
+    }
+}
