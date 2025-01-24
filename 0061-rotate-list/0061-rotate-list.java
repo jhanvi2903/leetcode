775a6595a7687d7091c9d4415aabc2eab1ea199b
Jhanvi Chaudhary <70269514+jhanvi2903@users.noreply.github.com>
2025-01-24 21:25:40 +0000

    Time: 0 ms (100%), Space: 43.1 MB (5.53%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.java
+++ 0061-rotate-list/0061-rotate-list.java
@@ -16,28 +16,33 @@ class Solution {
 
         if(head == null || head.next == null) return head;
         
-
+        // Find the length & the tail
         while(move != null) {
             length++;
             tail = move;
             move = move.next;
         }
         
-        k = k % length;
+        k = k % length; // k % length is required when k > length
+
+        // if k evaluates to 0, means no rotation. Return head
         if(k == 0) return head;
-        int stop = length - k;
+
+        // find the position to stop before the node which will be rotated
+        int stop = length - k; 
         int mover = 1;
 
+        // Traverse until we reach the stop node
         ListNode temp = head;
         while(mover != stop){
              temp = temp.next;
              mover++;
         }
 
-        ListNode hold = temp.next;
-        temp.next = null;
-        tail.next = head;
-        head = hold;
+        ListNode hold = temp.next; // hold the starting node from where the rotation starts
+        temp.next = null; // Disconnect and point to null
+        tail.next = head; // Point tail's next to head
+        head = hold; // New head
 
         return head;
     }
