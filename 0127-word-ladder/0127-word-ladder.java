3980e9e9c70747be7eef31ed7e01c89c00b04731
Jhanvi Chaudhary <70269514+jhanvi2903@users.noreply.github.com>
2025-01-28 09:44:28 +0000

    Time: 67 ms (80.6%), Space: 46.3 MB (67.05%) - LeetHub

---
+++ 0127-word-ladder/0127-word-ladder.java
@@ -0,0 +1,46 @@
+class Solution {
+    static class Pair{
+        String word;
+        int level;
+
+        Pair(String word, int level) {
+            this.word = word;
+            this.level = level;
+        }
+    }
+
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        HashSet<String> hs = new HashSet<>(wordList);
+        Queue<Pair> q = new LinkedList<>();
+        int levelCount = 1;
+
+         if(!hs.contains(endWord)) {
+            return 0;
+        }
+
+        q.offer(new Pair(beginWord, levelCount));
+        hs.remove(beginWord);
+
+        while(!q.isEmpty()) {
+            Pair p = q.poll();
+            String w = p.word;
+            levelCount = p.level;
+
+            if(w.equals(endWord)) return levelCount;
+            
+            for(int i = 0; i < w.length(); i++){
+                char[] wordArr = w.toCharArray();
+                for(char c = 'a'; c <= 'z'; c++) {
+                    wordArr[i] = c;
+                    String newWord = new String(wordArr);
+                    if(hs.contains(newWord)) {
+                        hs.remove(newWord);
+                        q.offer(new Pair(newWord, levelCount + 1));
+                    }
+                }
+            }
+        }
+
+      return 0;
+    }
+}
