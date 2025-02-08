e0c11f272bc623910b4942764ad9dd296567a852
Jhanvi Chaudhary <70269514+jhanvi2903@users.noreply.github.com>
2025-02-08 11:51:26 +0000

    Time: 7 ms (50.3%), Space: 45.9 MB (23.71%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,4 +1,6 @@
 class Solution {
+    // Topological Sort✨ Solution with\U0001f9e9Kahn’s Algorithm \U0001f4da(BFS)
+    //We need to figure out if we can complete all courses \U0001f393 by checking the prerequisites \U0001f517. If there’s a cycle \U0001f504 in the dependency graph, it’s impossible \U0001f6ab to finish all courses. However, if the graph is a DAG ✅ (Directed Acyclic Graph), we can complete all the courses.
     public boolean canFinish(int numCourses, int[][] prerequisites) {
        int[] indegree = new int[numCourses];
        List<List<Integer>> adj = new ArrayList<>();
@@ -8,10 +10,10 @@ class Solution {
        }
 
        for(int[] prerequisite : prerequisites) {
-           int u = prerequisite[0];
-           int v = prerequisite[1];
-           indegree[u]++;
-           adj.get(v).add(u);
+           int v = prerequisite[0];
+           int u = prerequisite[1];
+           indegree[v]++; // Adding indegree of the first node in array as there is an edge from v to u
+           adj.get(u).add(v);
        }
 
         Queue<Integer> q = new LinkedList<>(); 
@@ -34,6 +36,8 @@ class Solution {
             }   
         }
 
+        // If all the nodes are processed, therefore no cycle exist
+        // If all the nodes are not processed, so cycle exist
         return processedCourses == numCourses;
     }
 }
