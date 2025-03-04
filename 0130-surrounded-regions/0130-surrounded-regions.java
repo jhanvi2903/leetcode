656c6ba942a80e1bbda2b3f56302151e54635368
Jhanvi Chaudhary <70269514+jhanvi2903@users.noreply.github.com>
2025-03-04 13:51:29 +0000

    Time: 2 ms (86.2%), Space: 45.4 MB (73.29%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.java
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -3,33 +3,31 @@ class Solution {
     
     public void solve(char[][] board) {
 
-        if (board.length == 0 || board[0].length == 0)
-		return;
-	if (board.length < 2 || board[0].length < 2)
-		return;
+        if (board.length == 0 || board[0].length == 0 || board.length < 2 || board[0].length < 2) return;
+
         
         int row = board.length;
         int col = board[0].length;
-        int[][] vis = new int[row][col];
+        int[][] vis = new int[row][col]; // visited array
     
 
         for(int j = 0; j < row; j++) {
-             if(vis[j][0] != 1 && board[j][0] == 'O'){
+             if(vis[j][0] != 1 && board[j][0] == 'O'){ // 1st column
                 dfs(board, vis, j, 0);
             }
 
-           if(vis[j][col-1] != 1 && board[j][col-1] == 'O') {
+           if(vis[j][col-1] != 1 && board[j][col-1] == 'O') { // last column
                 dfs(board, vis, j, col-1);
             }
         }
 
         
         for(int i = 0; i < col; i++) {
-            if(vis[0][i] != 1 && board[0][i] == 'O') {
+            if(vis[0][i] != 1 && board[0][i] == 'O') { // 1st row
               dfs(board,vis, 0, i);  
             }
 
-            if(vis[row-1][i] != 1 && board[row-1][i] == 'O') {
+            if(vis[row-1][i] != 1 && board[row-1][i] == 'O') { // last row
                 dfs(board,vis,row-1, i);
             }
         }
@@ -45,13 +43,13 @@ class Solution {
     }
 
     public void dfs(char[][] board, int[][] vis, int row, int col) {
-        vis[row][col] = 1;
+        vis[row][col] = 1; // Mark visited
 
         for (int[] dir: dirs) {
             int i = dir[0] + row;
             int j = dir[1] + col;
             
-
+            // Move in 4 directions to mark 'O' as visited
             if (i >= 0 && i < board.length && j >=0 && j < board[0].length && board[i][j] == 'O' && vis[i][j] != 1) {
                 dfs(board, vis, i, j);
             }
