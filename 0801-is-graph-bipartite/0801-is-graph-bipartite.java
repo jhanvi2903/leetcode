d30840472b32a890daf4c348020f7c2ad89665d9
Jhanvi Chaudhary <70269514+jhanvi2903@users.noreply.github.com>
2025-03-05 15:13:40 +0000

    Time: 0 ms (100%), Space: 45.4 MB (19.36%) - LeetHub

--- 0801-is-graph-bipartite/0801-is-graph-bipartite.java
+++ 0801-is-graph-bipartite/0801-is-graph-bipartite.java
@@ -1,27 +1,16 @@
 class Solution {
     public boolean isBipartite(int[][] graph) {
       int rows = graph.length;
-    //   ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-      int[] color = new int[graph.length];
-
-    // for(int i = 0; i < rows; i++) {
-    //     adj.add(new ArrayList<>());
-    // }
-      
-    // for(int i = 0; i < rows; i++){
-    //     for(int j = 0; j < graph[i].length; j++) {
-    //         adj.get(i).add(graph[i][j]);
-    //     }
-    //   }
-    
+      int[] color = new int[graph.length]; // ~visited node array
 
     for(int i = 0; i < color.length; i++) {
         color[i] = -1;
     }
 
+    // To traverse all the disconnected components of a graph
     for(int i = 0; i < graph.length; i++) {
         if(color[i] == -1) {
-           if(dfs(color, graph, i, 0) == false) return false;
+           if(dfs(color, graph, i, 0) == false) return false; // early return
         }
     }
 
@@ -34,7 +23,7 @@ class Solution {
         for(int adjNode = 0; adjNode < adj[node].length; adjNode++) {
             int el = adj[node][adjNode];
             if(color[el] == -1) {
-                if(dfs(color, adj, el, 1 - col) == false) return false;
+                if(dfs(color, adj, el, 1 - col) == false) return false; // early return
             } else if(color[el] == col){
                 return false;
             }
