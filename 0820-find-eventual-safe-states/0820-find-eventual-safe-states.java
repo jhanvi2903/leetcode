1a922b858e6b2406ec313211e20fb742a3b6e288
Jhanvi Chaudhary <70269514+jhanvi2903@users.noreply.github.com>
2025-03-16 18:22:59 +0000

    Time: 25 ms (20.67%), Space: 55.4 MB (73.91%) - LeetHub

--- 0820-find-eventual-safe-states/0820-find-eventual-safe-states.java
+++ 0820-find-eventual-safe-states/0820-find-eventual-safe-states.java
@@ -1,7 +1,7 @@
 class Solution {
     public List<Integer> eventualSafeNodes(int[][] graph) {
         List<List<Integer>> adj = new ArrayList<>();
-         int[] indegree = new int[graph.length];
+         int[] outdegree = new int[graph.length];
 
         for(int i = 0; i < graph.length; i++) {
             adj.add(new ArrayList<>());
@@ -9,14 +9,14 @@ class Solution {
         
         for(int i = 0; i < graph.length; i++) {
             for(int j = 0; j < graph[i].length; j++) {
-                adj.get(graph[i][j]).add(i);
-                indegree[i]++;
+                adj.get(graph[i][j]).add(i); // incoming edges nodes
+                outdegree[i]++; // total outdegree edges
             }
         }
 
         Queue<Integer> q = new LinkedList<>();
-        for(int i = 0; i < indegree.length; i++) {
-            if(indegree[i] == 0) q.offer(i);
+        for(int i = 0; i < outdegree.length; i++) {
+            if(outdegree[i] == 0) q.offer(i);
         }
 
 
@@ -26,8 +26,8 @@ class Solution {
             result.add(node);
 
             for(int adjNode : adj.get(node)){
-                indegree[adjNode]--;
-                if(indegree[adjNode] == 0) q.offer(adjNode);
+                outdegree[adjNode]--;
+                if(outdegree[adjNode] == 0) q.offer(adjNode);
             }
         }
 
