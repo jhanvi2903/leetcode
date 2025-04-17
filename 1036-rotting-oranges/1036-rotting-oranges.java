19835d6439af525573ca682adc8748514d349b08
Jhanvi Chaudhary <70269514+jhanvi2903@users.noreply.github.com>
2025-04-17 15:19:38 +0100

    Time: 1 ms (100%), Space: 42.4 MB (75.49%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.java
+++ 1036-rotting-oranges/1036-rotting-oranges.java
@@ -1,4 +1,6 @@
 class Solution {
+    // Time Complexity: O(row × col)
+    // Space Complexity: O(row × col)
     public int orangesRotting(int[][] grid) {
         int row = grid.length;
         int col = grid[0].length;
