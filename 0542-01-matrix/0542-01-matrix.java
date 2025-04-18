9ed953ba44d6a60c2aa1b4034707f33ec7670bba
Jhanvi Chaudhary <70269514+jhanvi2903@users.noreply.github.com>
2025-04-18 19:45:18 +0100

    Time: 13 ms (84.26%), Space: 47.2 MB (64.62%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.java
+++ 0542-01-matrix/0542-01-matrix.java
@@ -5,15 +5,14 @@ class Solution {
     public int[][] updateMatrix(int[][] mat) {
         int row = mat.length;
         int col = mat[0].length;
-        int[][] vis = new int[row][col]; // visited matrix
-        int[][] dis = new int[row][col]; // distance matrix
         Queue<int[]> q = new LinkedList<>();
 
         for(int i = 0; i < row; i++){
             for(int j = 0; j < col; j++){
                 if(mat[i][j] == 0) {
-                    q.add(new int[] {i, j, 0});
-                    vis[i][j] = 1;
+                    q.add(new int[] {i, j});
+                }else {
+                    mat[i][j] = Integer.MAX_VALUE;
                 }
             }
         }
@@ -26,20 +25,66 @@ class Solution {
             int[] curr = q.poll();
             int i = curr[0];
             int j = curr[1];
-            int d = curr[2];
-            dis[i][j] = d; //Add distance in distance matrix
 
             for(int[] dir : directions) {
                 int x = i + dir[0];
                 int y = j + dir[1];
-                //Add in queue if the cell is not out of bound & never visited
-                if(x >= 0 && x < row && y >= 0 && y < col && vis[x][y] != 1) {
-                  q.add(new int[]{x, y, d + 1});
-                  vis[x][y] = 1;
+                //Add in queue if the cell is not out of bound 
+                if(x >= 0 && x < row && y >= 0 && y < col && mat[x][y] > mat[i][j] + 1) {
+                  q.add(new int[]{x, y});
+                   mat[x][y] = mat[i][j] + 1;
                 }
             }
         }
 
-        return dis;
+        return mat;
     }
 }
+
+
+// Less optimized in terms of space complexity
+// class Solution {
+//     // Using BFS:
+//     // Time complexity = O(row * col)
+//     // Space complexity = O(row * col)
+//     public int[][] updateMatrix(int[][] mat) {
+//         int row = mat.length;
+//         int col = mat[0].length;
+//         int[][] vis = new int[row][col]; // visited matrix
+//         int[][] dis = new int[row][col]; // distance matrix
+//         Queue<int[]> q = new LinkedList<>();
+
+//         for(int i = 0; i < row; i++){
+//             for(int j = 0; j < col; j++){
+//                 if(mat[i][j] == 0) {
+//                     q.add(new int[] {i, j, 0});
+//                     vis[i][j] = 1;
+//                 }
+//             }
+//         }
+      
+//       // Early return - if only 0's are present, it will decrease the execution time
+//        if(q.size() == row * col) return mat;
+
+//        int[][] directions = {{1,0}, {-1, 0}, {0, 1}, {0, -1}};
+//         while(!q.isEmpty()) {
+//             int[] curr = q.poll();
+//             int i = curr[0];
+//             int j = curr[1];
+//             int d = curr[2];
+//             dis[i][j] = d; //Add distance in distance matrix
+
+//             for(int[] dir : directions) {
+//                 int x = i + dir[0];
+//                 int y = j + dir[1];
+//                 //Add in queue if the cell is not out of bound & never visited
+//                 if(x >= 0 && x < row && y >= 0 && y < col && vis[x][y] != 1) {
+//                   q.add(new int[]{x, y, d + 1});
+//                   vis[x][y] = 1;
+//                 }
+//             }
+//         }
+
+//         return dis;
+//     }
+// }
