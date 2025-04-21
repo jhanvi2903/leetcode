0025eec1452be6010984008f0f8a547fe130ab7e
Jhanvi Chaudhary <70269514+jhanvi2903@users.noreply.github.com>
2025-04-21 16:55:00 +0100

    Time: 6 ms (75.69%), Space: 45 MB (83.31%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,6 +1,9 @@
 class Solution {
     // Topological Sort✨ Solution with\U0001f9e9Kahn’s Algorithm \U0001f4da(BFS)
     //We need to figure out if we can complete all courses \U0001f393 by checking the prerequisites \U0001f517. If there’s a cycle \U0001f504 in the dependency graph, it’s impossible \U0001f6ab to finish all courses. However, if the graph is a DAG ✅ (Directed Acyclic Graph), we can complete all the courses.
+
+// Time complexity: O(V + E)
+// Space complexity: O(V + E)
     public boolean canFinish(int numCourses, int[][] prerequisites) {
        int[] indegree = new int[numCourses];
        List<List<Integer>> adj = new ArrayList<>();
