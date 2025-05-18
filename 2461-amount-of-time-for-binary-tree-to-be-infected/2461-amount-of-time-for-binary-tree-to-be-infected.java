9181be7634242662aa3323ef4d5b120a13728327
Jhanvi Chaudhary <70269514+jhanvi2903@users.noreply.github.com>
2025-05-18 22:32:25 +0100

    Time: 83 ms (74.57%), Space: 68.4 MB (88.81%) - LeetHub

--- 2461-amount-of-time-for-binary-tree-to-be-infected/2461-amount-of-time-for-binary-tree-to-be-infected.java
+++ 2461-amount-of-time-for-binary-tree-to-be-infected/2461-amount-of-time-for-binary-tree-to-be-infected.java
@@ -13,6 +13,12 @@
  *     }
  * }
  */
+/* 
+Using BFS:
+T.C : O(n)
+S.C : O(n)
+*/
+
 class Solution {
     public int amountOfTime(TreeNode root, int start) {
         HashMap<TreeNode, TreeNode> childParentHashmap = new HashMap<>();
@@ -21,28 +27,33 @@ class Solution {
         queue.offer(root);
         TreeNode startNode = null;
 
+        // Step 1: BFS to populate child-parent and to find the start node
         while(!queue.isEmpty()) {
             TreeNode node = queue.poll();
 
+             // If current node is the one where infection starts, save it
             if(node.val == start) {
                 startNode = node;
             }
 
+            // If left child exists, map its parent and enqueue it
             if(node.left != null) {
                 childParentHashmap.put(node.left, node);
                 queue.offer(node.left);
             }
 
+            // If right child exists, map its parent and enqueue it
             if(node.right != null) {
                 childParentHashmap.put(node.right, node);
                 queue.offer(node.right);
             }
         }
 
-        HashSet<TreeNode> vis = new HashSet<>();
+        HashSet<TreeNode> vis = new HashSet<>(); // To track already infected nodes
         queue.offer(startNode);
-        int minutes = -1;
+        int minutes = -1; // Timer to track minutes until whole tree is infected
 
+        // Step 2: BFS to simulate infection spread from startNode
         while(!queue.isEmpty()) {
             int size = queue.size();
             minutes++;
@@ -51,14 +62,17 @@ class Solution {
                 TreeNode node = queue.poll();
                 vis.add(node);
 
+                // Try to infect the left child
                 if(node.left != null && !vis.contains(node.left)) {
                     queue.offer(node.left);
                 }
 
+                 // Try to infect the right child
                 if(node.right != null && !vis.contains(node.right)) {
                     queue.offer(node.right);
                 }
 
+                // Try to infect the parent
                 if(childParentHashmap.containsKey(node) && !vis.contains(childParentHashmap.get(node))) {
                     queue.offer(childParentHashmap.get(node));
                 }
