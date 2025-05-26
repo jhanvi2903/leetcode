5c6040b23f0b1bb8cf4c8d5c5ea6464287611a1f
Jhanvi Chaudhary <70269514+jhanvi2903@users.noreply.github.com>
2025-05-26 18:08:49 +0100

    Time: 0 ms (100%), Space: 45.2 MB (82.55%) - LeetHub

--- 0784-insert-into-a-binary-search-tree/0784-insert-into-a-binary-search-tree.java
+++ 0784-insert-into-a-binary-search-tree/0784-insert-into-a-binary-search-tree.java
@@ -13,19 +13,26 @@
  *     }
  * }
  */
+
+/*
+Intution: Since in a BST we only go in one direction at each step, I’ll keep track of the parent and traverse until I reach null — which is where the new node belongs. Then I attach it on the left or right of the last valid node.
+Time complexity: O(h)
+Space complexity: O(1)
+*/
 class Solution {
     public TreeNode insertIntoBST(TreeNode root, int val) {
-
         TreeNode newNode = new TreeNode(val);
 
         if(root == null) {
             return newNode;
         }
         
-        TreeNode prev = null;
+        TreeNode parentNode = null;
         TreeNode curr = root;
+
+        // Traverse the tree to find the correct insertion point
         while(curr != null) {
-             prev = curr;
+             parentNode = curr;
 
              if(val > curr.val) {
                 curr = curr.right;
@@ -34,10 +41,11 @@ class Solution {
              }
         }
 
-        if(prev.val < val) {
-            prev.right = newNode; 
+         // Attach the new node to the appropriate side of the parent node
+        if(parentNode.val < val) {
+            parentNode.right = newNode; 
         } else {
-            prev.left = newNode;
+            parentNode.left = newNode;
         }
 
         return root;
