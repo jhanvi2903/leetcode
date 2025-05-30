ef8bffe64c79f711383c22488801dd4d6001981f
Jhanvi Chaudhary <70269514+jhanvi2903@users.noreply.github.com>
2025-05-30 12:52:35 +0100

    Time: 2 ms (94.51%), Space: 45.6 MB (18.72%) - LeetHub

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.java
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.java
@@ -15,25 +15,48 @@
  */
 class Solution {
     public boolean findTarget(TreeNode root, int k) {
-        ArrayList<Integer> list = new ArrayList<>();
-        inOrder(root, list);
+        ArrayList<Integer> inorder = new ArrayList<>();
+        inorderTraversal(root, inorder);
 
-        for(int i = 0; i < list.size(); i++){
-            for(int j = i+1; j < list.size(); j++){
-                if(list.get(i) + list.get(j) == k) return true;
+        int i = 0;
+        int j = inorder.size()-1;
+
+        while(i < j) {
+            int sum = inorder.get(i) + inorder.get(j);
+            if(sum == k) {
+                return true;
+            } else if (sum < k) {
+                i++;
+            } else {
+                j--;
             }
         }
 
         return false;
     }
 
-    public void inOrder(TreeNode root, ArrayList<Integer> list){
-        if(root == null){
-            return;
-        }
+    private void inorderTraversal(TreeNode root, ArrayList<Integer> inorder) {
+        if(root == null) return;
 
-        inOrder(root.left, list);
-        list.add(root.val);
-        inOrder(root.right, list);
+        inorderTraversal(root.left, inorder);
+        inorder.add(root.val);
+        inorderTraversal(root.right, inorder);
     }
 }
+
+/*
+Approach 1: Inorder traversal will add elements in sorted order , then applying 2 pointer technique to find 2 elements = target
+Time complexity : O(n)
+Worst Space Complexity: O(n), for balanced tree : O(N) 
+Bonus : Follows the BST property as the elements are stored in sorted order in list and 2 pointer technique is used which is used on the sorted list
+
+Approach 2: Using inorder traversal and hashset, if we find target - curr element in hashset, then return true else add the curr element in hashset
+Time complexity : O(n)
+Worst Space Complexity: O(n), for balanced tree : O(N) 
+
+
+Approach 3 : Using 2 stack, one for storing min element and other for storing maximum element (Follows 2 pointer approach only) 
+Time complexity : O(n)
+Worst Space Complexity: O(n), but for balanced tree : O(h) as one halve is always eliminated at each level
+Bonus : Follows the BST property as the elements are stored in sorted order in list and 2 pointer technique is used which is used on the sorted list 
+*/
