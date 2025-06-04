73a8c8322ef8ae6a85d6e2eaf9510c19d941888e
Jhanvi Chaudhary <70269514+jhanvi2903@users.noreply.github.com>
2025-06-04 18:43:22 +0100

    Time: 0 ms (100%), Space: 40.4 MB (51.42%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -5,8 +5,7 @@ class Solution {
     }
 
     private int climb(int n, HashMap<Integer, Integer> memo) {
-        if(n == 0) return 1;
-        if(n < 0) return 0;
+        if(n == 0 || n == 1) return 1;
 
         if(memo.containsKey(n)) return memo.get(n);
 
