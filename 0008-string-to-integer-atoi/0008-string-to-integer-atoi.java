94dbb9d6ad4ffcbc797efd7d3b54852987581730
Jhanvi Chaudhary <70269514+jhanvi2903@users.noreply.github.com>
2025-06-05 13:14:26 +0100

    Time: 1 ms (100%), Space: 42.4 MB (81.02%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -2,28 +2,37 @@ class Solution {
     public int myAtoi(String s) {
         int n = s.length();
         int sign = 1;
-        long result = 0;
+        long result = 0; // Use long to detect overflow
         int idx = 0;
 
-        
+   
+        // 1. Skip whitespace     
         while(idx < n && s.charAt(idx) == ' ') {
             idx++;
         }
 
+         // 2. Handle optional sign
         if(idx < n && (s.charAt(idx) == '-' || s.charAt(idx) == '+')) {
             sign = s.charAt(idx) == '-' ? -1 : 1;
             idx++;
         }
 
+        // 3. Parse digits
         while(idx < n && Character.isDigit(s.charAt(idx))) {
-            int digit = s.charAt(idx) - '0';
+
+            // char to int conversion: 
+            //'0' unicode value is 48
+            // Similarly '1' unicode value is 49 and so on
+            int digit = s.charAt(idx) - '0'; 
 
             result = (result * 10) + digit;
 
+            // Rounding the result
             if(sign * result < Integer.MIN_VALUE) {
                 return Integer.MIN_VALUE;
             }
 
+            // Rounding the result
             if(sign * result > Integer.MAX_VALUE) {
                 return Integer.MAX_VALUE;
             }
@@ -31,6 +40,9 @@ class Solution {
             idx++;
         }
 
-        return (int)(sign*result);
+        return (int)(sign*result); // Converting long to int
     }
 }
+
+// Time Complexity : O(n) , where n is the length of string
+// Space Complexity : O(1)
