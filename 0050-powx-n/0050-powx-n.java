a9aa4eb3a159c8734eaae7ece95d71aa376522a3
Jhanvi Chaudhary <70269514+jhanvi2903@users.noreply.github.com>
2025-06-05 15:24:17 +0100

    Time: 0 ms (100%), Space: 41.8 MB (88.1%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -2,34 +2,22 @@ class Solution {
     public double myPow(double x, int n) {
 
         long N = n;
-        
-        // If the exponent is negative, invert x and make exponent positive
+
         if(N < 0) {
-            N = -N; // Make exponent positive
-            x = 1/x;        
+            N = -N;
+            x = 1 / x;
         }
 
         double result = 1;
-
         while(N != 0) {
-            // (n & 1) checks whether the last bit of n is 1
-            if((N & 1) == 1) {
-                result = result * x; // Adding one multiple of x as the current bit is 1
+            if(N % 2 == 1) {
+                result = result * x; 
             }
 
             x = x * x;
-
-            N = N >> 1; // equivalent to n >>= 1; discarding remainder
+            N = N/2;
         }
-        
-        return result;
+
+        return result;       
     }
 }
-
-/*
-The time complexity is O(log₂ |n|) because in each loop iteration, we reduce n by half using right shift (n >>= 1). So the number of steps is proportional to the number of bits in n, which is log₂ |n|.
-
-Space complexity : O(1)
-
-Note: We use long N = n; to safely handle the edge case when n == Integer.MIN_VALUE (which can't be negated as an int). Converting to long avoids overflow when we do N = -N.
-*/
