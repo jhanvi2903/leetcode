e5ccb00b2f43aca1612b579ba732e521fc6bbec8
Jhanvi Chaudhary <70269514+jhanvi2903@users.noreply.github.com>
2025-09-17 22:17:09 +0100

    Time: 6 ms (17.21%), Space: 45.7 MB (42.01%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.java
+++ 0137-single-number-ii/0137-single-number-ii.java
@@ -1,21 +1,18 @@
 class Solution {
     public int singleNumber(int[] nums) {
-
         int result = 0;
-        
-        for(int i = 0; i < 32; i++) {
-            int bitCount = 0;
-            for(int num : nums) {
-                if(((num >> i) & 1) == 1) {
-                    bitCount++;
-                }
-            }
+        HashMap<Integer, Integer> hm = new HashMap<>();
 
-            if(bitCount % 3 != 0) {
-                result = result | (1 << i);
+        for(int num : nums) {
+            hm.put(num, hm.getOrDefault(num, 0) + 1);
+        }
+
+        for(Map.Entry<Integer, Integer> num : hm.entrySet()) {
+            if(num.getValue() == 1) {
+                result = num.getKey();
             }
         }
 
-        return result;       
+        return result;    
     }
 }
