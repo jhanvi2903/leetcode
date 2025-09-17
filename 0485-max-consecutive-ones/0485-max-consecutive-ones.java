742e774a55a58c6d8fed87ad0bbbee78b94fd8ed
Jhanvi Chaudhary <70269514+jhanvi2903@users.noreply.github.com>
2025-09-17 11:05:05 +0100

    Time: 2 ms (95.32%), Space: 50.2 MB (47.73%) - LeetHub

--- 0485-max-consecutive-ones/0485-max-consecutive-ones.java
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.java
@@ -7,11 +7,13 @@ class Solution {
             if(nums[i] == 1) {
                count++;
             } else {
+                maxOnesCount = Math.max(maxOnesCount, count);
                 count = 0;
             }
-            maxOnesCount = Math.max(maxOnesCount, count);
         } 
 
+        maxOnesCount = Math.max(maxOnesCount, count);
+        
         return maxOnesCount;
     }
 }
