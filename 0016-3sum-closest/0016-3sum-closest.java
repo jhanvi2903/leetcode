c31773185931e1160a2aa0a8f3aac665fa4dc244
Jhanvi Chaudhary <70269514+jhanvi2903@users.noreply.github.com>
2025-09-18 23:04:17 +0100

    Time: 14 ms (69.19%), Space: 43.3 MB (46.08%) - LeetHub

---
+++ 0016-3sum-closest/0016-3sum-closest.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int threeSumClosest(int[] nums, int target) {
+        int result = Integer.MAX_VALUE;
+        Arrays.sort(nums);
+
+        for(int i = 0; i < nums.length; i++) {
+
+            int left = i + 1;
+            int right = nums.length - 1;
+
+            while(left < right) {
+                int currSum = nums[i] + nums[left] + nums[right];
+
+                if(Math.abs(target - currSum) < Math.abs(target - result)) {
+                    result = currSum;
+                }
+               
+
+                if(currSum > target) {
+                    right--;
+                } else if(currSum < target){
+                    left++;
+                } else {
+                    return result;
+                }
+            }
+        }
+
+       return result;        
+    }
+}
