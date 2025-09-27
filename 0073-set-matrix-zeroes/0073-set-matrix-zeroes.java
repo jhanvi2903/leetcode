e927b77d40a372529caaab672c335c6e850e6e92
Jhanvi Chaudhary <70269514+jhanvi2903@users.noreply.github.com>
2025-09-27 16:13:02 +0100

    Time: 0 ms (100%), Space: 46.1 MB (8.75%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -4,28 +4,28 @@ class Solution {
         int colLength = matrix[0].length;
 
 
-        boolean[] zerorows = new boolean[rowLength];
-        boolean[] zerocols = new boolean[colLength];
+        boolean[] zeroRows = new boolean[rowLength];
+        boolean[] zeroCols = new boolean[colLength];
 
         for(int row = 0; row < rowLength; row++) {
             for(int col = 0; col < colLength; col++) {
                 if(matrix[row][col] == 0) {
-                    zerorows[row] = true;
-                    zerocols[col] = true;
+                    zeroRows[row] = true;
+                    zeroCols[col] = true;
                 }
             }
         }
 
-        for(int row = 0; row < zerorows.length; row++) {
-            if(zerorows[row] == true) {
+        for(int row = 0; row < zeroRows.length; row++) {
+            if(zeroRows[row] == true) {
                 for(int col = 0 ; col < colLength; col++) {
                     matrix[row][col] = 0;
                 }
             }
         }
 
-         for(int col = 0; col < zerocols.length; col ++) {
-            if(zerocols[col] == true) {
+         for(int col = 0; col < zeroCols.length; col ++) {
+            if(zeroCols[col] == true) {
                 for(int row = 0 ; row < rowLength; row++) {
                     matrix[row][col] = 0;
                 }
