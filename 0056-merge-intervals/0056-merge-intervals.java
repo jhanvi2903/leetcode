81e11502d220891a2de49d3d688b955dcb34c053
Jhanvi Chaudhary <70269514+jhanvi2903@users.noreply.github.com>
2025-09-29 14:53:18 +0100

    Time: 9 ms (43.38%), Space: 46.3 MB (93.75%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,17 +1,22 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+        int[] prevInterval = intervals[0];
 
-        Arrays.sort(intervals, (a,b) -> Integer.compare(a[0], b[0]));
-        List<int[]> list = new ArrayList<>();
+        List<int[]> mergeList = new ArrayList<>();
+        mergeList.add(prevInterval);
 
-        for(int i = 0; i < intervals.length; i++){
-            if(list.size() == 0 || intervals[i][0] > list.get(list.size()-1)[1]){
-               list.add(new int[] {intervals[i][0], intervals[i][1]});
-            }else {
-               list.get(list.size()-1)[1] =  Math.max( list.get(list.size()-1)[1], intervals[i][1]); 
+        for(int i = 1; i < intervals.length; i++) {
+            int[] currInterval = intervals[i];
+
+            if(prevInterval[1] >= currInterval[0]) {
+                prevInterval[1] = Math.max(prevInterval[1], currInterval[1]);
+            } else {
+                mergeList.add(currInterval);
+                prevInterval = currInterval;
             }
         }
-
-        return list.toArray(new int[list.size()][]);      
+        
+        return mergeList.toArray(new int[mergeList.size()][]);
     }
 }
