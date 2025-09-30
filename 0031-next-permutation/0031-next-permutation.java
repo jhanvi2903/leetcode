82285f884b4e372a0be83c229a44d078ca6a3b3c
Jhanvi Chaudhary <70269514+jhanvi2903@users.noreply.github.com>
2025-09-30 18:52:04 +0100

    Time: 0 ms (100%), Space: 43 MB (89.55%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,41 +1,43 @@
 class Solution {
     public void nextPermutation(int[] nums) {
         int n = nums.length;
-        int index = -1;
+        int breakPointIndex = -1;
 
         for(int i = n - 2; i >= 0; i--) {
             if(nums[i] < nums[i + 1]) {
-                index = i;
+                breakPointIndex = i;
                 break;
             }
         }
 
-        if(index == - 1) {
+        if(breakPointIndex == - 1) {
             reverse(0, n - 1, nums);
             return;
         }
 
         
-        int j = n - 1;
-        while(nums[j] <= nums[index]) {
-            j--;
+        if(breakPointIndex >= 0) {
+            int j = n - 1;
+            while(nums[j] <= nums[breakPointIndex]) {
+                j--;
+            }
+            swap(breakPointIndex, j, nums);  
+            reverse(breakPointIndex + 1, n - 1, nums);
         }
-
-        swap(index, j, nums);  
-        reverse(index + 1, n - 1, nums);
+       
     }
 
-    private void swap(int low, int high, int[] nums) { 
-        int temp = nums[low];
-        nums[low] = nums[high];
-        nums[high] = temp;
+    private void swap(int left, int right, int[] nums) { 
+        int temp = nums[left];
+        nums[left] = nums[right];
+        nums[right] = temp;
     }
 
-    private void reverse(int low, int high, int nums[]) {
-        while(low < high) {
-            swap(low, high, nums);
-            low++;
-            high--;
+    private void reverse(int left, int right, int nums[]) {
+        while(left < right) {
+            swap(left, right, nums);
+            left++;
+            right--;
         }
 
     }
